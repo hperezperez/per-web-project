7994ab6e819e3cb392257408a6294c1eef6e6440
Humberto Perez <ferdinero121312@gmail.com>
2015-07-29 19:16:46 -0700

    I create file milstone-2

---
+++ milestone-2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+	<html>
+	 <head>
+		 <style>
+			 h1 {background-color:e0ffff;}
+			 p{background-color:#6495ed}
+			 div {
+				 background-color: #b0c4de;
+			 }
+		 </style>
+	 </head>
+	<body>
+		<h1> Persona</h1>
+	</body>
+</html>
+
