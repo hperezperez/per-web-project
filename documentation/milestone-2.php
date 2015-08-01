4f2a9d72b3b18b34141ca18a71463224e0482215
Humberto Perez <ferdinero121312@gmail.com>
2015-08-01 16:07:05 -0700

    I am adding persona and use case

---
+++ documentation/milestone-2.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+	<body background="pictures.jpg">
+		<h1>PERSONA 1</h1>
+		<h2> Susana  Lopez</h2>
+		<p> Age: 30</p>
+
+		<p> She is a house keeper for the Andaluz Hotel</p>
+		<p> She was born in Mexico but she now lives and work in Albuquerque since 2001</p>
+		<p> She is Married with three childrens. She likes Mexican products. SHe likes candies and every year
+			she organized a birthday party for each of his three kids. their ages are 4, 6 and 9 years old.</p>
+		<p> She also send money back home to her mom in Mexico</p>
+
+
+		Goals:
+		<li>Send Money to Mexico</li>
+		<li> Buy mexican candy</li>
+		<li>BUy pinatas </li>
+		<h1>  USE CASE</h1>
+		<p> Susan Lopez has received a telephone call from her uncle in Mexico. </p>
+		<p> Her aunt is sick. So, she needs to sent money to Mexico. She wants a good exchange rate, </p>
+		<p>She wants to choose  the rigth company with the rigth service charge  </p>
+		<p>It 's 7: 25 am on Tuesday morning. she can not go out</p>
+		<p>   </p>
+		<p> Susan  is watching the site on his tablet at the laundry room</p>
+		<p> She is using crome</p>
+		<p> She had a short break of 10 min. It is an outline of Susan's interactions  with the site:</p>
+      <li> 1. type the name of my web site.Clicks call to action that navigatesto the services Page</li>
+		<li> 2. views first to companies of 3in services gallery.  She finds aa good option </li>
+		<li> She goes to the "about Money transfer companies page via the main navigation in the header</li>
+		<li> 3. She looks carefully into "about MTC page" for about  two minutes</li>
+		<li>  she choose one company  because that companies pay in Walmart Mexico.</li>
+		<li> she stop and get the addres information and telephone number  of the store because</li>
+		<li> all money transfer tracssaction must to be cash in th physical store by law</li>
+			<h2>  Joseph Martinez</h2>
+		<p> Age  22</p>
+
+		<p>Joseph is a construction worker. He was born and raise in New mexico.  His father is mexican and his mother is Guatemalan</p>
+		<p> HE needs a good phone and resistant  because he uses in his work.  He does not want to spent a lot  of money</p>
+		<p>  He needs a unlimited plan  talk and tex  with data for 40.ddls or 50 ddls </p>
+		<p> He also talk to Mexico with his uncle and nices</p>
+		<p> He also send money to Mexico ocasonality </p>
+		<p> He does not have bank acount because he overdraft very often</p>
+		<h2> Goals</h2>
+		,<li>  buy a Cellular phone with good  data plan and good price</li>
+		<li>  Send Money to his relative in Guatemala</li>
+		<li>   Cash his payroll check</li>
+</html>
