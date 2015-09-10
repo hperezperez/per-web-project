023e1118b175e09fef8e787be5e119b56b2f1b90
Humberto Perez <ferdinero121312@gmail.com>
2015-09-10 15:25:13 -0700

    We put a hamburger to header

--- lib/footer.php
+++ lib/footer.php
@@ -1,7 +1,7 @@
 <footer>
 <div class="container">
 	<div class="row">
-		<div class="col-md-3">About Us</div>
+		<div class="col-md-3"><a href="/aboutus"></a>About Us</div>
 		<div class="col-md-6"></div>
 		<div class="col-md-3 align-right">
 			<a href="#"><i class="fa fa-twitter social-media"></i></a>
--- lib/header.php
+++ lib/header.php
@@ -1,27 +1,30 @@
 <div class="container">
-	<div class="row">
-		<div class="col-md-3">
-			<a class="home-link" href=".">
-				<span class=" title">
-				<img class="logo" src="../images/bandera.jpg" alt="Arriba Mexico Logo"/>
-				Arriba Mexico
-				</span>
+	<nav class="navbar">
+
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-menu-collapse" aria-expanded="false">
+				<span class="sr-only">Main Menu</span>
+				<i class="fa fa-bars fa-lg"></i>
+			</button>
+			<a class="navbar-brand home-link" href="/">
+				<span class="title"><img class="logo" src="../images/bandera.jpg" alt="Arriba Mexico Logo"/>Arriba Mexico</span>
 			</a>
 		</div>
-		<div class="col-md-3">
 
-		</div>
-		<div class="col-md-6">
-			<ul class="nav nav-pills pull-right">
+		<!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse" id="main-menu-collapse">
+			<ul class="nav navbar-nav navbar-right">
 				<?php if($locale === "en_US") { ?>
-				<li role="presentation"><a href="<?php echo $_SERVER["PHP_SELF"] . "?locale=es_MX"; ?>">Espa&ntilde;ol</a> </li>
+					<li role="presentation"><a href="<?php echo $_SERVER["PHP_SELF"] . "?locale=es_MX"; ?>">Espa&ntilde;ol</a> </li>
 				<?php } else { ?>
-				<li role="presentation"><a href="<?php echo $_SERVER["PHP_SELF"] . "?locale=en_US"; ?>">English</a> </li>
+					<li role="presentation"><a href="<?php echo $_SERVER["PHP_SELF"] . "?locale=en_US"; ?>">English</a> </li>
 				<?php } ?>
+				<li role="presentation"><a href="/">Home</a> </li>
 				<li role="presentation"><a href="/services">Services</a></li>
 				<li role="presentation"><a href="/products">Products</a></li>
 				<li role="presentation"><a href="/aboutus">About Arriba Mexico</a></li>
 			</ul>
 		</div>
-	</div>
+	</nav>
 </div>
